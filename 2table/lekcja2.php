249764c394ce516c640ab6020334d2b1704b43ad
gromula <gromulab@gmail.com>
2017-03-12 12:05:40 +0100

    add

--- 2table/lekcja2.php
+++ 2table/lekcja2.php
@@ -1,2 +1,12 @@
 <?php
 
+$tbl = array();//Ta zamienna $tablice bedzie od teraz tablica
+$tbl[0] = 1;
+$tbl[1] = 2;
+$tbl[2] = 3;
+$tbl[3] = 4;
+$tbl[4] = 10;
+
+ //nazwa_tablicy[element] = wartosc;
+
+//* element to równie¿ mo¿e byc nazywany indeksem
