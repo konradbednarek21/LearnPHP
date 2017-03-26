cbfc9325dd62b778f6e2d687059ac7d4da847926
Artur <Artur@Artur-Komputer>
2017-03-26 08:49:17 +0200

    Zadanie if

--- if else/zadania/1.php
+++ if else/zadania/1.php
@@ -1,1 +1,19 @@
 <?php
+
+$pierwszaLiczba = 8;
+
+$drugaLiczba = 6;
+
+echo $pierwszaLiczba. "<br>";
+
+echo $drugaLiczba. "<br>";
+
+if($pierwszaLiczba >> $drugaLiczba)
+{
+	echo "Pierwsza liczba jest wiêksza";
+}
+
+else
+{
+	echo "Druga liczba jest wiêksza";
+}
