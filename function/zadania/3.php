d223b9835090598e8deacd9ad4dca8af1f311b38
Artur <Artur@Artur-Komputer.mshome.net>
2017-04-09 16:18:49 +0200

    Poprawione zadanie 3

--- function/zadania/3.php
+++ function/zadania/3.php
@@ -2,8 +2,8 @@
 function getDowolnyElementTablicy() 
 {
 $tbl = array('PierwszyElm', 'DrugiElm', 'TrzeciElm', 'CzwartyElm'); 
-$DowolnyEmementTablicy = rand(0, 4);
-return $DowolnyElementTablicy();
+$DowolnyElementTablicy = rand(0, 4);
+return $DowolnyElementTablicy;
 }
 
 $DowolnyElm = "Który element tablicy " . getDowolnyElementTablicy();
