879a277424dbfe7ec323681c4663a94db9fdb766
gromula <gromulab@gmail.com>
2017-05-14 10:20:13 +0200

    zadanie kolejne

---
+++ klasy/zadanie.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Created on 14 May 2017 10:16:44
+* error prefix
+* @author gromula
+*
+*/
+
+//hermetyzacja/dziedziczenie/polimorfizm
+
+//1. Utwórz klas samochód która posiada metody jedz(), stop(), tankuj(); // w kazdej metodzie bdzie zwraca³a tekst + nazwa metody
+//np: echo "jade";
+
+//2. Przerób ta klase na klase abstrakcyjna
+//3. Utwórz klase Audi która dziedziczy po klasie abstr. samochód i nadpisuje jej metody.
+
+
+?>
