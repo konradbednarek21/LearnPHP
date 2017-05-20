c4cb50b906568f110335200c7a16b10affe3fdfc
Artur <Artur@Artur-Komputer.mshome.net>
2017-05-20 13:39:03 +0200

    zaddom

---
+++ klasy/zaddomzrob.php
@@ -0,0 +1,38 @@
+<?php
+
+include 'ZadanieDomowe.php';
+
+abstract class Przesylka
+{
+	public function nadaj()
+	{
+		echo "nadaje przesy³kê";	
+	}
+	
+	public function odbierz()
+	{
+		echo "odbieram przesy³kê";
+	}
+
+	public function edytuj()
+	{
+		echo "edytujê przesy³kê";
+	}
+}
+
+class PrzesylkaFirmowa extends Przesylka
+{
+	public function nadaj()
+	{
+		echo "Nadajê przesy³kê firmow¹";
+	}
+	
+	public function odbierz()
+	{
+		echo "Odbieram przesy³kê firmow¹";
+	}
+}
+
+$PrzeFirm = new PrzesylkaFirmowa;
+
+echo PrzeFirm;
